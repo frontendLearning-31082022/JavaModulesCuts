9730e96ca5aa7a0d9a229b67ba6eb1aa1f29f45c
testoccgg <testoccgg@gmail.com>
2022-11-04 19:11:07 +0300

    all apps start point

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
 import ExecutionMeasuring.SimpleExecMeasure;
+import SQL.Mysql;
+import SQL.MysqlQuery;
+import SQL.SQLpartsDownLoad;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
-        //1
+        //1 execute Code
         SimpleExecMeasure.main(null);
+
+        //2 CodeForRead
+        Mysql mysql=null;
+        try {mysql=new Mysql("dbName");} catch (Exception e) {}
+
+        //3 CodeForRead
+        MysqlQuery mysqlQuery=new MysqlQuery("name","url",null);
+        mysql.executeQuery(mysqlQuery,false);
+
+        //4 CodeForRead  --- LoadList from db - first 2000 , and while all data tranfered
+        SQLpartsDownLoad sqLpartsDownLoad=new SQLpartsDownLoad("SELECT * etc","nameDB","parent local folder path for set ratings to articles");
+
 
 
     }
